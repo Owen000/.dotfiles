e5d1e937e84395dd862e568f3e4881de021f2d3a
Owen <67721175+Owen000@users.noreply.github.com>
2024-02-25 13:30:46 -0600

    Delete nvim directory

--- nvim/after/plugin/indent_blankline.lua
+++
@@ -1,9 +0,0 @@
-require("ibl").setup {
-    exclude = {
-        filetypes = {"dashboard"},
-        buftypes = {"terminal"},
-    },
-}
-
-vim.cmd('highlight IndentBlanklineContextChar guifg=#3c3836')
-
